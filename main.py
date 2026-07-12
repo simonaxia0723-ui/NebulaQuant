cc61a52189692ab7cdce360ff85497cf7e2dd61d
Simona Xia <simonaxia74@gmail.com>
2026-07-12 20:39:46 +0800

    Added stock statistics and price visualization

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 print("Welcome to nebulaQuant!")
 ticker = input("Enter a stock ticker: ").upper()
 print("You selected: ", ticker)
 
-data = yf.download(ticker, period="5y", )
-print(data)
+data = yf.download(ticker, period="5y")
+
+print(data.head())
+print(data.shape)
+
+close_prices = data["Close"]
+print(close_prices.head())
+
+print("highest closing price:", close_prices.max())
+
+print("lowest closing price:", close_prices.min())
+
+print("mean of closing price:", close_prices.mean())
+
+print("number of trading days:", len(close_prices))
+
+#show the graph
+close_prices.plot()
+plt.show()
+plt.title("stock price")
+plt.xlabel("time")
+plt.ylabel("closing price")
