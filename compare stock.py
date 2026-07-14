53abc8126ef916e8fb7ff8c50e493620ba2e149e
Simona Xia <simonaxia74@gmail.com>
2026-07-14 21:12:44 +0800

    Normalized stock comparison

--- compare stock.py
+++ compare stock.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 
 tickers = input("enter stock tickers: ").split()
 time_period = input("enter timer period: ")
+
 fig, ax = plt.subplots()
+
 for ticker in tickers:
     data = yf.download(ticker, period = time_period)
     close_prices = data["Close"].squeeze()
-    ax.plot(close_prices.index, close_prices, label=ticker)
-ax.set_title("stock price")
+    normalized_prices = close_prices / close_prices.iloc[0] * 100
+    ax.plot(close_prices.index, normalized_prices, label=ticker)
+
+ax.set_title("normalized prices")
 ax.set_xlabel("time")
-ax.set_ylabel("closing price")
+ax.set_ylabel("normalized prices")
 ax.legend()
 plt.show()
